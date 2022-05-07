5bd9e64576b1df3c2a18acd002bb5423ddcc06d8
vadim <vadimir29@gmail.com>
2022-05-07 15:48:00 +0200

    Third  commit

--- script/script.js
+++ script/script.js
@@ -1,6 +1,19 @@
-let buttonDel = document.querySelector(".C");
-let display = document.querySelector(".result");
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let result = false;
 
-buttonDel.addEventListener("click", function () {
-  display.innerHTML = "0";
+const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
+const operators = ["+", "-", "*", "/"];
+
+const display = document.querySelector(".result");
+const buttons = document.querySelector(".buttons");
+const allClear = document.querySelector(".C");
+
+allClear.addEventListener("click", function () {
+  firstNum = "";
+  secondNum = "";
+  operator = "";
+  result = false;
+  display.innerHTML = 0;
 });
